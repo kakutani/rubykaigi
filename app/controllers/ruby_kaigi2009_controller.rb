e3e65ccf436fab4f60f31d9e7503cc2d97205b20
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-02-08 15:08:07 +0900

    import presite designed by norio.

--- app/controllers/ruby_kaigi2009_controller.rb
+++ app/controllers/ruby_kaigi2009_controller.rb
@@ -1,7 +1,10 @@
 class RubyKaigi2009Controller < ApplicationController
-  caches_page :index
+  caches_page :page
 
   def index
   end
 
+  def page
+    render :template => "ruby_kaigi2009/index"
+  end
 end
