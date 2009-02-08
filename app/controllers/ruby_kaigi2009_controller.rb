6e48f4c4a8f65cc774f7ad5068e7f0a2407dc5b2
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-02-08 19:22:13 +0900

    added debug output

--- app/controllers/ruby_kaigi2009_controller.rb
+++ app/controllers/ruby_kaigi2009_controller.rb
@@ -5,15 +5,16 @@ class RubyKaigi2009Controller < ApplicationController
 
   def page
     page_name = params[:page] || "index"
-    @page_title = I18n.t(page_name)
     render :template => "ruby_kaigi2009/#{page_name}_#{I18n.locale}"
   end
 
   private
   def specify_locale
     availables = %w{ja en}
+    logger.debug("preferred_languages: #{request.user_preferred_languages.join(',')}")
     preferred = request.preferred_language_from(availables)
     compatible = request.compatible_language_from(availables)
+    logger.debug("preferred: #{preferred}, compatible: #{compatible}")
     I18n.locale = params[:locale] || preferred || compatible
   end
 
