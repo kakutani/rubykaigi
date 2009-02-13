79c50ed3409bbac04fac6113fd60e919d2d00e58
moro <moronatural@gmail.com>
2009-02-13 10:21:12 +0900

    refactor

--- config/initializers/ruby19monkey.rb
+++ config/initializers/ruby19monkey.rb
@@ -1,46 +1,50 @@
-module CorrectResponseContentLength
-  def self.included(base)
-    base.alias_method_chain :set_content_length!, :string_bytesize
-  end
-  private
-  def set_content_length_with_string_bytesize!
-    if "1.8.7 or lator".respond_to?(:bytesize)
-      unless body.respond_to?(:call) || (status && status[0..2] == '304')
-        self.headers["Content-Length"] ||= body.bytesize
+module StringBytesize
+  module ResponseContentLength
+    def self.included(base)
+      base.alias_method_chain :set_content_length!, :string_bytesize
+    end
+    private
+    def set_content_length_with_string_bytesize!
+      if "1.8.7 or lator".respond_to?(:bytesize)
+        unless body.respond_to?(:call) || (status && status[0..2] == '304')
+          self.headers["Content-Length"] ||= body.bytesize
+        end
+      else
+        set_content_length_without_string_bytesize!
       end
-    else
-      set_content_length_without_string_bytesize!
     end
   end
 end
 
-module EncodeOutputBuffer
-  def self.included(base)
-    base.alias_method_chain :output_buffer=, :encoding
-  end
-
+module ForceEncoding
   @@template_encoding = "UTF-8"
   mattr_accessor :template_encoding
 
-  def output_buffer_with_encoding=(out)
-    out && out.force_encoding(template_encoding)
-    self.output_buffer_without_encoding = out
-  end
-end
+  module OutputBuffer
+    def self.included(base)
+      base.alias_method_chain :output_buffer=, :encoding
+    end
 
-module EncodeRenderedTemplate
-  def self.included(base)
-    base.alias_method_chain :render_template, :encode
+    def output_buffer_with_encoding=(out)
+      out && out.force_encoding(ForceEncoding.template_encoding)
+      self.output_buffer_without_encoding = out
+    end
   end
 
-  def render_template_with_encode(v, l = {})
-    render_template_without_encode(v, l).tap do |out|
-      out && out.force_encoding("UTF-8")
+  module RenderResult
+    def self.included(base)
+      base.alias_method_chain :render_template, :encode
+    end
+
+    def render_template_with_encode(v, l = {})
+      render_template_without_encode(v, l).tap do |out|
+        out && out.force_encoding(ForceEncoding.template_encoding)
+      end
     end
   end
 end
 
-ActionController::AbstractResponse.send(:include, CorrectResponseContentLength)
-ActionView::Base.send(:include, EncodeOutputBuffer)
-ActionView::Template.send(:include, EncodeRenderedTemplate)
+ActionController::AbstractResponse.send(:include, StringBytesize::ResponseContentLength)
+ActionView::Base.send(:include, ForceEncoding::OutputBuffer)
+ActionView::Template.send(:include, ForceEncoding::RenderResult)
 
