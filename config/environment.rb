aab208c3c767ac9c9bb14bd74d2c309a82ca80e0
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-03-17 01:03:01 +0900

    update to Rails 2.3.2

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.0' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -32,7 +32,8 @@ Rails::Initializer.run do |config|
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-  config.plugins = [:ruby19_monkey, :footnotes, :http_accept_language ]
+  #config.plugins = [:ruby19_monkey, :footnotes, :http_accept_language ]
+  config.plugins = [:ruby19_monkey, :http_accept_language ]
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
