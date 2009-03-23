6912a15a719bb8aa2a6487fb79534654cfb1dbf1
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-03-23 09:56:08 +0900

    make green

--- spec/controllers/public_releases_routing_spec.rb
+++ spec/controllers/public_releases_routing_spec.rb
@@ -4,17 +4,17 @@ describe PublicReleasesController do
   describe "route generation" do
     it "should map #show with locale" do
       route_for(:controller => "public_releases", :action => "show",
-                :page_name => "index", :locale => "ja", :year=>2009).should == "/2009/ja"
+                :page_name => "index", :locale => "ja", :year=> "2009").should == "/2009/ja"
     end
-  
+
     it "should map #show with locale" do
       route_for(:controller => "public_releases", :action => "show",
-                :page_name => "index", :locale => "en", :year=>2009).should == "/2009/en"
+                :page_name => "index", :locale => "en", :year=> "2009").should == "/2009/en"
     end
 
     it "should map #show without locale" do
       route_for(:controller => "public_releases", :action => "show",
-                :page_name => "index", :year=>2009).should == "/2009"
+                :page_name => "index", :year=> "2009").should == "/2009"
     end
   end
 
@@ -24,7 +24,7 @@ describe PublicReleasesController do
       params_from(:get, "/2009/ja/").should ==
         @base.merge(:locale => "ja", :page_name => "index")
     end
-  
+
     it "should generate params for '/2009/ja/cfp'" do
       params_from(:get, "/2009/ja/cfp").should ==
         @base.merge(:locale => "ja", :page_name => "cfp")
