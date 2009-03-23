f7ac2147a6fe036ead6ee3f16b4f6d95b1676400
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-03-23 20:55:45 +0900

    set default locale to en

--- config/environment.rb
+++ config/environment.rb
@@ -11,6 +11,7 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.i18n.default_locale = 'en'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
