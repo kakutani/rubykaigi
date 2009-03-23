e150ad0f4c17bad0216314ea12b86ec1af02f936
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-03-24 02:00:44 +0900

    fix account related bugs :-<

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,12 @@
 class AccountsController < ApplicationController
   def new
+    @account = Account.new
+  end
+
+  def create
+    @account = Account.new(:identity_url =>  session[:openid_identifier])
+    @account.save!
+    redirect_to root_path
   end
 
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,4 @@
 class Account < ActiveRecord::Base
+  validates_presence_of :identity_url
+  validates_uniqueness_of :identity_url
 end
