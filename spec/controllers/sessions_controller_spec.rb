d9f6f71b4d77b8bc4f883dfe1d9353e17cecbd43
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-03-24 16:48:32 +0900

    make green

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -15,17 +15,4 @@ describe SessionsController do
     end
   end
 
-  describe "GET 'create'" do
-    it "should be successful" do
-      get 'create'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'destroy'" do
-    it "should be successful" do
-      get 'destroy'
-      response.should be_success
-    end
-  end
 end
--- spec/views/accounts/new.html.erb_spec.rb
+++ spec/views/accounts/new.html.erb_spec.rb
@@ -4,9 +4,5 @@ describe "/accounts/new" do
   before(:each) do
     render 'accounts/new'
   end
-  
-  #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/accounts/new])
-  end
+
 end
