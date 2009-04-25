da30b432d20a418b76c14a30d01606a95c830d5b
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-04-26 08:57:10 +0900

    rspec-rails 1.2.4

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,8 +21,8 @@ config.action_controller.allow_forgery_protection    = false
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
-config.gem "rspec", :lib => false, :version => ">= 1.2.2"
-config.gem "rspec-rails", :lib => false, :version => ">= 1.2.2"
+config.gem "rspec", :lib => false, :version => ">= 1.2.4"
+config.gem "rspec-rails", :lib => false, :version => ">= 1.2.4"
 config.gem "cucumber", :version => ">= 0.2.0"
 config.gem "thoughtbot-factory_girl", :lib => 'factory_girl', :source => 'http://gems.github.com'
 config.gem "rr"
