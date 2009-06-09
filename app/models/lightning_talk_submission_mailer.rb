bb4da3d913978920008b97871220b2805259699a
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-06-09 16:17:00 +0900

    extract e-mail address(rubykaigi.org) into configatron.

--- app/models/lightning_talk_submission_mailer.rb
+++ app/models/lightning_talk_submission_mailer.rb
@@ -2,8 +2,8 @@ class LightningTalkSubmissionMailer < ActionMailer::Base
   def submission_received(submission)
     subject 'RubyKaigi2009 Lightning Talks Submission Received'
     recipients submission.email
-    from '2009@rubykaigi.org'
-    bcc '2009@rubykaigi.org'
+    from       configatron.action_mailer.from
+    bcc        configatron.action_mailer.bcc if Rails.env.production?
     sent_on Time.now
     body :submission => submission
   end
