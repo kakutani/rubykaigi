98749ff1b839c467b88f93bedf6be50e97fbc1ca
SHIDARA Yoji <dara@shidara.net>
2009-06-09 17:55:59 +0900

    誕生石認証をつけてみた

--- app/models/lightning_talk_submission.rb
+++ app/models/lightning_talk_submission.rb
@@ -3,4 +3,5 @@ class LightningTalkSubmission < ActiveRecord::Base
   validates_presence_of :email
   validates_presence_of :title
   validates_presence_of :abstract
+  validates_acceptance_of :the_birthstone_of_july, :accept => 'ruby', :message => I18n.t('lt.is_ruby')
 end
