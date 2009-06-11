eda7d74f55626729fb17b950eb169be565091ac8
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-06-11 11:45:32 +0900

    use --drb w/ spork

--- spec/views/exchange_tickets/index.html.erb_spec.rb
+++ spec/views/exchange_tickets/index.html.erb_spec.rb
@@ -7,6 +7,6 @@ describe "/exchange_tickets/index" do
 
   #Delete this example and add some real ones or delete this file
   it "should tell you where to find the file" do
-    response.should have_tag('h2', "Exchange Ticket (Reissue)")
+    response.should have_tag('h2')
   end
 end
