a9172f048c590a77570919e643a12de16b749653
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-06-16 18:49:10 +0900

    How about lightning talk submission with registration?

--- spec/views/paypal/thanks.html.erb_spec.rb
+++ spec/views/paypal/thanks.html.erb_spec.rb
@@ -1,27 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/paypal/thanks" do
-  before(:each) do
-    stub(@paypal = PaypalTransaction.new) {
-      item_number { 'rk09c' }
-      txn_id { '1234567890ABCDEFG' }
-      receipt_id { '1111-2222-3333-4444' }
-      last_name { 'Kakutani' }
-      first_name { 'Shintaro' }
-      payer_email { 'kakutani@rubykaigi.org' }
-      verify { 'VERIFIED' }
-    }
-    assigns[:paypal] = @paypal
-    pending
-    render 'paypal/thanks'
+
+  context "en" do
+    before do
+      I18n.locale = 'en'
+      assigns[:notified_transaction_id] = '1234356709ABCDEF!'
+      render 'paypal/thanks'
+    end
+    subject { response }
+    it { should have_tag('h3', 'Thank you for your registration.') }
   end
 
-  it "should show thank you" do
-    response.should have_tag('h3', "Thank you for your registration.")
+  context "ja" do
+    before do
+      I18n.locale = 'ja'
+      assigns[:notified_transaction_id] = '1234356709ABCDEF!'
+      render 'paypal/thanks'
+    end
+    subject { response }
+    it { should have_tag('h3', '登録ありがとうございます。') }
   end
-
-  it "should show exchange ticket" do
-    response.should have_tag('h4', 'Exchange Ticket')
-  end
-
 end
