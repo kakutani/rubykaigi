287a75c56b07c095e4bd53f242f0603a7208864a
SAWADA Tadashi <moc.liamg.cesare@gmail.com>
2009-06-17 23:35:22 +0900

    set schema to talks.

--- db/migrate/20090617132546_create_talks.rb
+++ db/migrate/20090617132546_create_talks.rb
@@ -1,7 +1,14 @@
 class CreateTalks < ActiveRecord::Migration
   def self.up
     create_table :talks do |t|
-
+      t.string :title
+      t.string :speaker
+      t.string :language
+      t.text :summary
+      t.integer :year
+      t.integer :room
+      t.datetime :starts_at
+      t.datetime :ends_at
       t.timestamps
     end
   end
