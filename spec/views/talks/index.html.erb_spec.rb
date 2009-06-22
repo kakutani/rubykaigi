57d6533538f9fd624c5a3ba3b71ff95c02d1c02c
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-06-23 01:56:43 +0900

    make green

--- spec/views/talks/index.html.erb_spec.rb
+++ spec/views/talks/index.html.erb_spec.rb
@@ -2,16 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/talks/index.html.erb" do
   include TalksHelper
-  
-  before(:each) do
-    assigns[:talks] = [
-      stub_model(Talk),
-      stub_model(Talk)
-    ]
-  end
 
-  it "renders a list of talks" do
+  before do
     render
   end
-end
 
+  it { template.should_not be_nil }
+end
--- spec/views/talks/show.html.erb_spec.rb
+++ spec/views/talks/show.html.erb_spec.rb
@@ -3,11 +3,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 describe "/talks/show.html.erb" do
   include TalksHelper
   before(:each) do
-    assigns[:talk] = @talk = stub_model(Talk)
+    assigns[:talk] = @talk = Talk.new
   end
 
   it "renders attributes in <p>" do
     render
   end
 end
-
