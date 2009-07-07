80745f307fb8990b08394408ca56440cd4a43f13
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-07-07 14:12:47 +0900

    add student?

--- app/controllers/admin/paypal_transactions_controller.rb
+++ app/controllers/admin/paypal_transactions_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'fastercsv'
 class Admin::PaypalTransactionsController < AdminController
   def download
@@ -6,10 +7,11 @@ class Admin::PaypalTransactionsController < AdminController
       wants.csv {
         paypal_txns = PaypalTransaction.find_all_by_item_number(item_number)
         csv = FasterCSV.generate(:col_sep => "\t") do |c|
-          c << ["row", "item_number", "4digit", "txn_id", "email", "last_name", "first_name", "memo"]
+          c << ["row", "item", "4digits", "txn_id", "email", "last_name", "first_name", "student?", "memo"]
           paypal_txns.each_with_index do |txn, idx|
             memo = txn.memo && txn.memo.gsub(/\n/, "/")
-            c << [idx + 1, txn.item_number, txn.txn_id[0,4], txn.txn_id, txn.payer_email, txn.last_name, txn.first_name, memo]
+            is_student = (txn.memo =~ /学生/ ? "学" : " ")
+            c << [idx + 1, txn.item_number, txn.txn_id[0,4], txn.txn_id, txn.payer_email, txn.last_name, txn.first_name, is_student, memo]
           end
         end
         send_data(csv, :filename => "#{item_number}.csv",
